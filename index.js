8a510eaf3167cd7ff9766fa677e9c18d94e9f51b
Branden Palmer <brandenpalmer08@gmail.com>
2023-09-11 20:46:09 +0700

    add files

---
+++ index.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+axios
+  .get('google.com')
+  .then(res => {
+    console.log(`statusCode: ${res.status}`);
+    console.log(res);
+  })
+  .catch(error => {
+    console.error(error);
+  });
