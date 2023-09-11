f680df54e87d2791a0070485f8f59154979ba627
Branden Palmer <brandenpalmer08@gmail.com>
2023-09-11 23:58:29 +0700

    finish server

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const axios = require('axios');
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
 
-axios
-  .get('google.com')
-  .then(res => {
-    console.log(`statusCode: ${res.status}`);
-    console.log(res);
+let notFoundPage
+fs.readFile('./404.html', (e, data) => {
+  if (e) return console.error('error page not found')
+  notFoundPage = data
+})
+
+http.createServer((req, res) => {
+  const { pathname } = url.parse(req.url, true)
+  const pagePath = pathname.length > 1 ? `.${pathname}.html` : './index.html'
+  fs.readFile(pagePath, (e, data) => {
+    if (e) {
+      res.writeHead(418, { 'Content-Type': 'text/html' });
+      res.write(notFoundPage)
+      return res.end()
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data)
+    return res.end()
   })
-  .catch(error => {
-    console.error(error);
-  });
+}).listen(8080)
